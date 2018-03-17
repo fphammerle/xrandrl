8b7681436a100cb9e337ab1202605d85174b3126
Fabian Peter Hammerle <fabian@hammerle.me>
2018-03-17 22:52:39 +0100

    list names of all available outputs

---
+++ README.md
@@ -0,0 +1,7 @@
+# xrandrl
+
+## Requirements
+
+```{sh}
+sudo apt-get install python3 libx11-dev libxrandr-dev
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+import glob
+
+version = '0.1'
+
+setup(
+    name='xrandrl',
+    version=version,
+    description='',
+    author='Fabian Peter Hammerle',
+    author_email='fabian@hammerle.me',
+    url='https://git.hammerle.me/fphammerle/xrandrl',
+    # download_url =
+    # 'https://github.com/fphammerle//tarball/{}'.format(version),
+    keywords=[],
+    classifiers=[],
+    scripts=glob.glob('scripts/*'),
+    install_requires=[],
+    tests_require=['pytest'],
+)
